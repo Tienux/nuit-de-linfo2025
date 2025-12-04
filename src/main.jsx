50fd1897500dbe773b73aedd0b7dee867e4e7359
Mathischvn <mathischauvin256@gmail.com>
2025-12-05 00:37:05 +0100

    fix deploy pipline + fix main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,10 @@ import App from './App.jsx'
 import PlayGame from './PlaySnacke.jsx'
 import './index.css'
 
+const BASE_NAME = "/nuit-de-linfo2025";
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <BrowserRouter>
+        <BrowserRouter basename={BASE_NAME}>
             <Routes>
                 <Route path="/" element={<App />} />
                 <Route path="/play" element={<PlayGame />} />
