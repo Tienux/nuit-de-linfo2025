0bc46d9e1fc4a676d38fc2b4d097386f73367f91
Mathischvn <mathischauvin256@gmail.com>
2025-12-05 03:10:25 +0100

    add some fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
-import PlayGame from './PlaySnacke.jsx'
 import './index.css'
 
-const BASE_NAME = "/nuit-de-linfo2025";
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <BrowserRouter basename={BASE_NAME}>
-            <Routes>
-                <Route path="/" element={<App />} />
-                <Route path="/play" element={<PlayGame />} />
-            </Routes>
-        </BrowserRouter>
+        <App />
     </React.StrictMode>,
 )
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [react()],
-    base: "/nuit-de-linfo2025/",
 });
 
