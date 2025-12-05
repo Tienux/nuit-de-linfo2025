a692f648134e20370b3e677660e2716a3b718739
Mathischvn <mathischauvin256@gmail.com>
2025-12-05 04:04:47 +0100

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [react()],
-    base: process.env.NODE_ENV === "production" ? "/nuit-de-linfo2025/" : "/"
+    base: "/"
 });
 
